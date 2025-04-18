f82d9dd0af0b19fa19ff95a5b247a2106f79688c
Omi Bajja <omicron@DESKTOP-0FTMOVP.>
2025-04-18 11:56:58 +0200

    apostrophe

--- app/components/QuoteSection.tsx
+++ app/components/QuoteSection.tsx
@@ -5,7 +5,7 @@ export default function QuoteSection() {
         <div className="text-[#68d5e3] mb-6 text-xl">OUR PHILOSOPHY</div>
         <blockquote className="text-3xl font-light leading-relaxed mb-8">
           &ldquo;<span className="text-[#68d5e3]">Hey Seeker!</span> is built
-          for the curious and the brave, for those who believe there's more to
+          for the curious and the brave, for those who believe there&apos;s more to
           discover, ready to dive into the infinite possibilities of
           blockchain.&rdquo;
         </blockquote>
